1faafde45d7d13c6bdb36c1b04ad699b4384a524
amitshankar2 <36152153-amitshankar2@users.noreply.replit.com>
2025-02-14 01:24:30 +0000

    Agent query: You should now see the registration/login page. Can you confirm if you see the form where you can register as a hairstylist?
    Add registration page redirect to /auth
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/7d5b0c4b-5884-404b-bfe5-7ceafdb16f7c/d1af42e9-dd9f-464e-80ba-f8ee7b1c7aa3.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,8 +12,11 @@ import BookAppointment from "@/pages/book-appointment";
 function Router() {
   return (
     <Switch>
+      <Route path="/">
+        <Redirect to="/auth" />
+      </Route>
       <Route path="/auth" component={AuthPage} />
-      <ProtectedRoute path="/" component={StylistDashboard} />
+      <ProtectedRoute path="/dashboard" component={StylistDashboard} />
       <Route path="/book" component={BookAppointment} />
       <Route component={NotFound} />
     </Switch>
